591ee4cabb505af0b8d73ec8594ee1dd6fc721a8
zelie-delhotal <gatien.delhotal@orange.fr>
2025-03-25 01:01:32 +0100

    must decide if I recode a putstr for return value

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: gdelhota <gdelhota@student.42perpigna      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 19:25:54 by gdelhota          #+#    #+#             */
-/*   Updated: 2024/11/13 19:07:58 by gdelhota         ###   ########.fr       */
+/*   Updated: 2024/11/13 20:44:05 by gdelhota         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,59 +16,65 @@
 #include <stdio.h>
 #include <unistd.h>
 
-static void	ft_puthexa(unsigned long nb, int is_uppercase, int is_address)
+static int	ft_puthexa(unsigned long nb, char format)
 {
 	char	*base;
+	int		chars_written;
 
-	base = "0123456789ABCDEF";
-	if (!is_uppercase)
+	chars_written = 0;
+	if (format == 'X')
+		base = "0123456789ABCDEF";
+	else
 		base = "0123456789abcdef";
-	if (is_address)
-		write(1, "0x", 2);
+	if (format = 'p')
+		chars_written += write(1, "0x", 2);
 	if (nb < 16)
-		ft_putchar_fd(base[nb], 1);
+		chars_written += write(1, &base[nb], 1);
 	else
 	{
 		ft_puthexa(nb / 16, is_uppercase, 0);
 		ft_puthexa(nb % 16, is_uppercase, 0);
 	}
+	return (chars_written);
 }
 
 int	ft_printf(const char *format, ...)
 {
 	va_list	ap;
 	size_t	i;
+	int		n;
 
 	i = -1;
+	n = 0;
 	va_start(ap, format);
 	while (format[++i])
 	{
 		if (format[i] != '%')
-			write(1, &format[i], 1);
+			n += write(1, &format[i], 1);
 		else if (format[++i] == 'c')
-			ft_putchar_fd((char) va_arg(ap, int), 1);
+			n += write(1, (char) va_arg(ap, int), 1);
 		else if (format[i] == 's')
 			ft_putstr_fd((char *) va_arg(ap, char *), 1);
 		else if (format[i] == 'p')
-			ft_puthexa((unsigned long) va_arg(ap, long), 0, 1);
+			n += ft_puthexa((unsigned long) va_arg(ap, long), format[i]);
 		else if (format[i] == 'd' || format[i] == 'i')
 			ft_putnbr_fd((int) va_arg(ap, int), 1);
 		else if (format[i] == 'u')
 			ft_putnbr_fd((unsigned int) va_arg(ap, int), 1);
 		else if (format[i] == 'x' || format[i] == 'X')
-			ft_puthexa((unsigned int) va_arg(ap, int), format[i] == 'X', 0);
+			n += ft_puthexa((unsigned int) va_arg(ap, int), format[i]);
 		else if (format[i] == '%')
-			write(1, "%", 1);
+			n += write(1, "%", 1);
 	}
 	va_end(ap);
-	return (0);//returns bytes written
+	return (n);
 }
 
-/*int	main(void)
+int	main(void)
 {
 	char *s = "test";
 	printf("printf:\n");
-	printf("%p\n", s);
+	printf("%s\n", s);
 	printf("ft_printf:\n");
-	ft_printf("%p\n", s);
-}*/
+	ft_printf("%s\n", s);
+}
